ec687909870a711077b49b2b78bef46b27fd916f
utzcoz <utzcoz@outlook.com>
2021-08-14 20:54:42 +0800

    Use robolectric-4.6.1 to test open uncompressed asset file
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- app/src/test/java/com/robolectric/issue5442/ExampleUnitTest.java
+++ app/src/test/java/com/robolectric/issue5442/ExampleUnitTest.java
@@ -1,17 +1,33 @@
 package com.robolectric.issue5442;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.google.common.io.CharStreams;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+@RunWith(AndroidJUnit4.class)
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void openFd_shouldProvideFileDescriptorForAsset() throws IOException {
+        Context context = ApplicationProvider.getApplicationContext();
+        AssetManager assetManager = context.getAssets();
+        AssetFileDescriptor assetFileDescriptor = assetManager.openFd("assetsHome.txt");
+        assertThat(CharStreams.toString(new InputStreamReader(
+                assetFileDescriptor.createInputStream(), Charset.forName("UTF-8"))))
+                .isEqualTo("assetsHome!");
+        assertThat(assetFileDescriptor.getLength()).isEqualTo(11);
     }
 }
